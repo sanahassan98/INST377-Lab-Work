b9338ed2fe8d571ffb4b1d1b02ec906e58c495ba
sanahassan98 <64567467+sanahassan98@users.noreply.github.com>
2022-02-18 22:32:37 -0500

    tetris
    tetrimino movements

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -1,7 +1,162 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+   
+    //The Terominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2], 
+        [width, width+1, width+2, width*2+2],
+        [1,width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+   
+     const zTetromino = [
+        [0, width, width+1, width*2+1], 
+        [width+1, width+2, width*2, width*2+1],
+        [0,width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+    const tTetromino = [
+       [1,width,width+1,width+2],
+       [1,width+1,width+2,width*2+1],
+       [width,width+1,width+2,width*2+1],
+       [1,width,width+1,width*2+1]
+     ]
+   
+     const oTetromino = [
+       [0,1,width,width+1],
+       [0,1,width,width+1],
+       [0,1,width,width+1],
+       [0,1,width,width+1]
+     ]
+   
+     const iTetromino = [
+       [1,width+1,width*2+1,width*3+1],
+       [width,width+1,width+2,width+3],
+       [1,width+1,width*2+1,width*3+1],
+       [width,width+1,width+2,width+3]
+     ]
+   const theTetrominoes =[lTetromino, zTetromino, tTetromino,oTetromino,iTetromino]
+   
+   let currentPosition = 4
+   let currentRotation = 0
+
+   //randomly select a Tetromino and its first rotation
+   let random = Math.floor(Math.random()*theTetrominoes.length)
+   let current = theTetrominoes[random][0]
+
+     //draw the first rotation in the first tetromino
+function draw() {
+    current.forEach(index => { 
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+}
+ //undraw the Tetrimino
+ function undraw(){
+     current.forEach(index => {
+         squares[currentPosition + index].classList.remove('tetromino')
+     })
+ }
+
+//make the tetromino fall down every second
+timerId = setInterval(moveDown, 1000)
+
+//assign functions to keyCodes
+function control(e) {
+    if(e.keyCode === 37) {
+       moveleft()
+    }else if (e.keyCode === 38) {
+        //rotate
+    } else if (e.keycode === 39) {
+       moveRight()
+    } else if (e.keyCode === 40) {
+        moveDown()
+    }  
+}
+document.addEventListener('keyup', control)
+
+
+
+//fall down function
+function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+
+   //freeze function
+   function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+       //start a new tetromino falling
+       random = Math.floor(Math.random() * theTetrominoes.length)
+       current = theTetrominoes[random][currentRotation]
+       currentPosition = 4
+       draw()
+   }
+   }
+//move the tetromino left, unless it is at the edge or there is a blockage
+function moveleft() {
+    undraw()
+    const isAtleftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if(!isAtleftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition +=1
+    }
+
+draw()
+
+}
+
+//move the tetromino right, unless is at the edge or there is a blockage
+
+function moveRight(){
+    undraw()
+    const isatRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+    if(!isAtrightEdge) currentPosition +=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition -=1
+    }
+    draw()
+
+}
 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ client/lab_4/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#next-button").onclick = (e) => {
+    alert("next button was clicked");
+};
+document.querySelector("#previous-button").onclick = (e) => {
+    alert("previous button was clicked");
+};
