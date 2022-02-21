e08d3df306b4719e3adbb7decb9b377ace5f4d9e
sanahassan98 <64567467+sanahassan98@users.noreply.github.com>
2022-02-20 21:06:23 -0500

    lab 4 done
    lab 4 done

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,6 +1,24 @@
+
+const first = document.querySelector(".carousel-item.visible");
+const last = document.querySelector(".carousel-item:last-of-type");
+const prev = document.querySelector("#previous-button");
+
+let current = first;
+
+// Event listeners
 document.querySelector("#next-button").onclick = (e) => {
-    alert("next button was clicked");
+  prev.style.fontWeight = "normal";
+  prev.style.backgroundColor = "lightgray";
+  current.classList.remove("visible");
+  current = current.nextElementSibling ? current.nextElementSibling : first;
+  current.classList.add("visible");
 };
 document.querySelector("#previous-button").onclick = (e) => {
-    alert("previous button was clicked");
+  //css font-weight => js fontWeight
+  prev.style.fontWeight = "bold";
+  prev.style.backgroundColor = "orange";
+  current.classList.remove("visible");
+  current = current.previousElementSibling ? current.previousElementSibling : last;
+  
+  current.classList.add("visible");
 };
