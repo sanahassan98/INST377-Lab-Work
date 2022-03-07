35ebe88457eb7057939644f686d44a49ce325486
sanahassan98 <64567467+sanahassan98@users.noreply.github.com>
2022-03-06 22:50:54 -0500

    lab 5 done
    lab 5 completed

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -7,14 +7,24 @@ let current = first;
 
 // Event listeners
 document.querySelector("#next-button").onclick = (e) => {
-  prev.style.fontWeight = "normal";
+  
+  
+  
+  
+  prev.style.fontWeight = "bold";
   prev.style.backgroundColor = "lightgray";
+  
   current.classList.remove("visible");
   current = current.nextElementSibling ? current.nextElementSibling : first;
   current.classList.add("visible");
+
 };
 document.querySelector("#previous-button").onclick = (e) => {
   //css font-weight => js fontWeight
+  
+  
+
+
   prev.style.fontWeight = "bold";
   prev.style.backgroundColor = "orange";
   current.classList.remove("visible");
