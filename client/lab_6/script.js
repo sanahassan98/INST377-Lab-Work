4158ca6dc66e3c852a621f30d1584cfde7b01ec2
sanahassan98 <64567467+sanahassan98@users.noreply.github.com>
2022-04-13 22:47:38 -0400

    lab 6 halfway done
    halfway done

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,13 +1,24 @@
+function dataHandler(anArray) {
+  console.table(anArray); // this is called "dot notation"
+  // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+  // it contains all 1,000 records we need
+}
+
+
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
+  const button = document.querySelector('button');
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  button.style.setProperty('display', 'none')
+  if (arrayFromJson.data.length > 0) {
+    button.style.setProperty('display', 'block');
+  }
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+    dataHandler(arrayFromjson.data);
   });
 }
 
